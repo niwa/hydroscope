e3b4fb0d141276ef65e43ae7a13fab23d68222f2
Matt Wilkins <matt.wilkins@niwa.co.nz>
2026-02-09 14:46:04 +1300

    Allow setting date/time range for each variable

--- bin/sourcedata.py
+++ bin/sourcedata.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 from PyQt6.QtGui import QRegularExpressionValidator
-from PyQt6.QtCore import QRegularExpression
+from PyQt6.QtCore import QRegularExpression, QDateTime
 from PyQt6.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
@@ -25,6 +25,7 @@ from PyQt6.QtWidgets import (
     QDialogButtonBox,
     QGridLayout,
     QGroupBox,
+    QDateTimeEdit,
 )
 
 
@@ -40,13 +41,16 @@ class SourceData:
         self.series = None  # the selected variable as a pd.Series
         self.units = None  # the units of selected variable as a string
         self.var = None  # measuring this var
+        self.timerange = [None, None]  # possibly restrict time range to this for this var
         self.dims = {}  # measuring var with these dims
 
     def read_fn(self, fn):
 
         self.series = None
+        self.units = None
         self.var = None
         self.dims = {}
+        self.timerange = [None, None]
 
         # get the data and set vars
         self.fn = fn
@@ -64,12 +68,13 @@ class SourceData:
                         f"Unsupported file format, please read Help menu.\nOriginal error:\n{exp1}\nFallback error assuming DD/MM/YYYY:\n{exp2}"
                     )
             if pd.infer_freq(df.index) is None:
-                raise ValueError(f"First column doesn't have uniform frequency")
+                raise ValueError("First column doesn't have uniform frequency")
             self.vars = df.columns.tolist()
             try:
                 df.apply(pd.to_numeric, errors="raise")
-            except Exception as exp:
+            except Exception:
                 raise ValueError("All defined values must be numeric")
+            df["time"] = df.index
             self.data = df
             self.v2d = {v: [] for v in self.vars}
             self.v2u = {v: None for v in self.vars}
@@ -78,6 +83,15 @@ class SourceData:
             # unfortunately topnet doesn't write correct streamq files, time_bnd doesn't have attrs set
             # self.data = ds = xr.open_dataset(fn)
             self.data = ds = xr.open_dataset(fn, drop_variables="time_bnd")
+            # rename and time like dim or coord to time
+            for dim in ds.dims:
+                if dim.lower() in ("time", "date", "datetime"):
+                    if dim != "time":
+                        ds = ds.rename({dim: "time"})
+            for coord in ds.coords:
+                if coord.lower() in ("time", "date", "datetime"):
+                    if coord != "time":
+                        ds = ds.rename({coord: "time"})
             self.v2d = {
                 v: [d for d in da.dims if not re.search(r"time|date", d, re.IGNORECASE)]
                 for v, da in ds.data_vars.items()
@@ -99,9 +113,6 @@ class SourceData:
     def get_vars(self):
         return self.vars
 
-    def get_dims(self):
-        return self.dims
-
     def get_d2type(self):
         return self.d2type
 
@@ -115,6 +126,7 @@ class SourceData:
         self.series = None
         self.dims = {}
         self.units = None
+        self.timerange = [None, None]
 
         # when clearing set_var gets called with None, so wipe it out
         if not v:
@@ -127,16 +139,41 @@ class SourceData:
         self.var = v
         self.__set_series(update_units=True)
 
-    def set_dims(self, d):
-        """d is a dict of dim to value"""
+    def get_dims(self):
+        return self.dims
+
+    def set_dims(self, d2v):
+        """d2v is a dict of dim to value"""
 
         self.series = None
-        if not d:
+        if not d2v:
             self.dims = {}
             return
 
-        self.dims = d
+        if not all([d in self.d2vals and v in self.d2vals[d] for d, v in d2v.items()]):
+            return
+
+        self.dims = d2v
         self.__set_series()
+
+    def get_timerange(self):
+        return self.timerange
+
+    def set_timerange(self, rg):
+        """rg is a list [Timestamp, Timestamp]"""
+        self.series = None
+
+        if rg is None or rg == [None, None]:
+            return
+
+        # if a valid time range
+        if (
+            self.data is not None
+            and self.data["time"].min() <= rg[0] < rg[1] <= self.data["time"].max()
+        ):
+            self.data["time"].values.min()
+            self.timerange = rg
+            self.__set_series()
 
     def __set_series(self, update_units=False):
         if not self.var:
@@ -145,14 +182,18 @@ class SourceData:
         if update_units:
             self.set_units(self.v2u.get(self.var, None))
 
-        # no dimensions to worry about
+        # no dimensions, probably csv
         if not self.v2d[self.var]:
             self.series = self.data[self.var]
+        elif self.dims and all(d in self.dims for d in self.v2d[self.var]):
+            self.series = self.data[self.var].sel(self.dims).to_series()
+        else:
             return
 
-        # we have dimensions to worry about, do we have them set?
-        if self.dims and all(d in self.dims for d in self.v2d[self.var]):
-            self.series = self.data[self.var].sel(self.dims).to_series()
+        # if there is a time range, select on that
+        if self.timerange:
+            start, end = self.timerange
+            self.series = self.series.loc[start:end]
 
     def get_series(self):
         return self.series
@@ -183,6 +224,17 @@ class SourceDataWidget(QGroupBox):
         if v and v in self.sd.get_vars() and self.vars_cb.findText(v) != -1:
             self.vars_cb.setCurrentText(v)
 
+        # if dims set, inform sd
+        d = self.parent.cp["DEFAULT"].get(f"{self.title}_dims")
+        if d:
+            self.sd.set_dims(json.loads(d))
+
+        # if timerange set, inform sd
+        rg = self.parent.cp["DEFAULT"].get(f"{self.title}_timerange")
+        if rg:
+            start, end = [pd.Timestamp(s) for s in json.loads(rg)]
+            self.sd.set_timerange([start, end])
+
     def init_ui(self):
 
         # Horizontal layout
@@ -207,7 +259,12 @@ class SourceDataWidget(QGroupBox):
 
         # Dimensions button
         self.dims_btn = btn = QPushButton("Dimensions")
-        self.dims_btn.clicked.connect(self.set_dims)
+        self.dims_btn.clicked.connect(self.__act_dims)  # store in config
+        hbox.addWidget(btn)
+
+        # Datetime range button
+        self.timerange_btn = btn = QPushButton("Time range")
+        self.timerange_btn.clicked.connect(self.__act_timerange)  # user
         hbox.addWidget(btn)
 
         hbox.addStretch()
@@ -302,12 +359,17 @@ class SourceDataWidget(QGroupBox):
     def __act_var(self, idx):
         """When user changes var we save it"""
         self.parent.cp["DEFAULT"][f"{self.title}_var"] = self.vars_cb.itemText(idx)
+        self.parent.save_config()
         self.__set_var(self.vars_cb.itemText(idx))
 
     def __set_units(self, u):
         self.sd.set_units(u)
 
-    def set_dims(self):
+    def __set_dims(self, d2v):
+        """dim_name to selected/entered value as strings"""
+        self.sd.set_dims(d2v)
+
+    def __act_dims(self):
         vname = self.vars_cb.currentText()
         if not vname:
             QMessageBox.warning(self, "No variable", "Please select a variable")
@@ -324,7 +386,35 @@ class SourceDataWidget(QGroupBox):
         if dialog.exec() == QDialog.DialogCode.Accepted:
             selected_values = dialog.get_values()
             self.parent.cp["DEFAULT"][f"{self.title}_dims"] = json.dumps(selected_values)
-            self.sd.set_dims(selected_values)
+            self.parent.save_config()
+            self.__set_dims(selected_values)
+
+    def __set_timerange(self, rg):
+        """rg is [start Timestamp, end Timestamp]"""
+        self.sd.set_timerange(rg)
+
+    def __act_timerange(self):
+        s = self.sd.get_series()
+        if s is None:
+            QMessageBox.warning(self, "No variable", "File/variable/dimension must be all set")
+            return
+
+        # see if there are defaults saved.  should get from self.sd
+        rg = self.sd.get_timerange()
+
+        # get the full time index from sd
+        index = self.sd.data["time"]
+        if hasattr(index, "values"):
+            index = pd.to_datetime(index.values)
+
+        dialog = TimeRangeSelectorDialog(index, start=rg[0], end=rg[1], parent=self.parent)
+        if dialog.exec() == QDialog.DialogCode.Accepted:
+            selected_values = dialog.get_values()
+            self.parent.cp["DEFAULT"][f"{self.title}_timerange"] = json.dumps(
+                [selected_values[0].isoformat(), selected_values[1].isoformat()]
+            )
+            self.parent.save_config()
+            self.__set_timerange(selected_values)
 
 
 class DimensionSelectorDialog(QDialog):
@@ -404,3 +494,84 @@ class DimensionSelectorDialog(QDialog):
             v = widget.currentText() if isinstance(widget, QComboBox) else widget.text()
             vals[dim] = self.d2type[dim](v)
         return vals
+
+
+class TimeRangeSelectorDialog(QDialog):
+    def __init__(self, index, start=None, end=None, parent=None):
+        """
+        Parameters
+        ----------
+        index: Datetime index
+            Restrict selectable range
+
+        start: pd.Timestamp (optional)
+            Initial from value
+
+        end: pd.Timestamp (optional)
+            Initial to value
+
+        """
+        super().__init__(parent)
+        self.setWindowTitle("Select time range")
+
+        layout = QVBoxLayout()
+        grid = QGridLayout()
+        grid.addWidget(QLabel("From"), 0, 0)
+        grid.addWidget(QLabel("To"), 1, 0)
+
+        self.from_edit = QDateTimeEdit()
+        self.to_edit = QDateTimeEdit()
+        for w in (self.from_edit, self.to_edit):
+            w.setCalendarPopup(True)
+            w.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
+
+        # index is garanteed to have at least two items
+        idx_min = index.min()
+        idx_max = index.max()
+        qmin = QDateTime(idx_min)
+        qmax = QDateTime(idx_max)
+        self.from_edit.setMinimumDateTime(qmin)
+        self.from_edit.setMaximumDateTime(qmax)
+        self.to_edit.setMinimumDateTime(qmin)
+        self.to_edit.setMaximumDateTime(qmax)
+        if start is None:
+            start = idx_min
+        if end is None:
+            end = idx_max
+
+        self.from_edit.setDateTime(QDateTime(start))
+        self.to_edit.setDateTime(QDateTime(end))
+        grid.addWidget(self.from_edit, 0, 1)
+        grid.addWidget(self.to_edit, 1, 1)
+
+        # need to set up buttons now because if line_edit is used then disable Ok button until input
+        buttons = QDialogButtonBox(
+            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+        )
+        self.ok_button = buttons.button(QDialogButtonBox.StandardButton.Ok)
+        self.ok_button.setEnabled(True)
+
+        layout.addLayout(grid)
+        buttons.accepted.connect(self.accept)
+        buttons.rejected.connect(self.reject)
+        layout.addWidget(buttons)
+        self.setLayout(layout)
+
+        # validate range
+        self.from_edit.dateTimeChanged.connect(self._validate)
+        self.to_edit.dateTimeChanged.connect(self._validate)
+        self._validate()
+
+    def _validate(self):
+        """Disable OK if from > to."""
+        start = self.from_edit.dateTime()
+        end = self.to_edit.dateTime()
+        self.ok_button.setEnabled(start <= end)
+
+    def get_values(self):
+        """
+        Return a tuple (start datetime, end datetime)
+        """
+        start = pd.Timestamp(self.from_edit.dateTime().toPyDateTime())
+        end = pd.Timestamp(self.to_edit.dateTime().toPyDateTime())
+        return (start, end)
